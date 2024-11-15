915b8acbe9990ad10d1147ac9701b0b3ed51baf5
RSWilli <bartel.wilhelm@gmail.com>
2024-11-15 15:22:00 +0100

    remove unused global maps

--- glib/register.go
+++ glib/register.go
@@ -9,5 +9,3 @@ var registerMutex sync.RWMutex
 var registeredTypes = make(map[string]Type)
 
 var registeredClasses = make(map[C.gpointer]GoObjectSubclass)
-var registeredClassTypes = make(map[C.gpointer]Type)
-var registeredIfaces = make(map[C.gpointer][]Interface)
