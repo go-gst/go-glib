1090b9b24cf4c9f950b48de1fcff2f3962f08131
RSWilli <bartel.wilhelm@gmail.com>
2026-01-28 09:51:22 +0100

    add windows build github action stage

--- buildAll.sh
+++ buildAll.sh
@@ -2,15 +2,14 @@
 
 # this script builds all go packages in the current directory
 
-packages=$(go list ./...)
+packages=$(go list ./pkg/...)
 for package in $packages; do
-    echo "building $package"
-    if ! go build -o /dev/null "$package"; then
-        read -p "Build failed for $package. Do you want to continue? (Y/n): " choice
-        choice=${choice:-y}
-        if [[ "$choice" != "y" && "$choice" != "Y" ]]; then
-            echo "Exiting build process."
-            exit 1
-        fi
-    fi
+    echo "building $package" 
+    go build -o /dev/null "$package" || exit 1
 done
+
+# examples=$(go list ./examples/...)
+# for ex in $examples; do
+#     echo "building example $ex" 
+#     go build -o /dev/null "$ex" || exit 1
+# done
