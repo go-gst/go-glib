2ee73774ee6d0bec7a233198b99a4714d96297ad
Alexander Bakker <ab@alexbakker.me>
2026-07-18 11:36:44 +0200

    Add basic Go -> C support for GValueArray

--- pkg/gobject/v2/valuearray.go
+++ pkg/gobject/v2/valuearray.go
@@ -6,7 +6,10 @@ package gobject
 // #include <glib-object.h>
 // #include <glib.h>
 import "C"
-import "unsafe"
+import (
+	"runtime"
+	"unsafe"
+)
 
 var TypeValueArray Type = Type(C.g_value_array_get_type())
 
@@ -18,8 +21,31 @@ func init() {
 //
 // See https://docs.gtk.org/gobject/struct.ValueArray.html for more details.
 //
-// Since GValueArray is deprecated, this type is provided for compatibility purposes only, and only for C->Go marshaling.
+// Since GValueArray is deprecated, this type is provided for compatibility purposes only.
+//
+// A nil ValueArray is marshaled as an empty GValueArray in C.
 type ValueArray []any
+
+var _ GoValueInitializer = ValueArray(nil)
+
+// GoValueType implements GoValueInitializer.
+func (a ValueArray) GoValueType() Type {
+	return TypeValueArray
+}
+
+// SetGoValue implements GoValueInitializer.
+func (a ValueArray) SetGoValue(v *Value) {
+	nativeArray := C.g_value_array_new(C.guint(len(a)))
+
+	for _, elem := range a {
+		elemValue := NewValue(elem)
+		C.g_value_array_append(nativeArray, elemValue.native())
+		runtime.KeepAlive(elemValue)
+	}
+
+	C.g_value_take_boxed(v.native(), C.gconstpointer(unsafe.Pointer(nativeArray)))
+	runtime.KeepAlive(v)
+}
 
 func marshalValueArray(p unsafe.Pointer) (interface{}, error) {
 	nativeArray := (*C.GValueArray)(C.g_value_get_boxed((*C.GValue)(p)))
